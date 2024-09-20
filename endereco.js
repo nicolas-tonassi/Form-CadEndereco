9643d8270f86f3b34f2f9068de71fbd77dec0b76
nicolas-tonassi <nicolas.tonassi@alunoescolassociais.org.br>
2024-09-20 07:29:15 -0300

    colocando o valor da API dentro do campo do formulário

--- endereco.js
+++ endereco.js
@@ -18,3 +18,11 @@ const limparFormulario = () =>{
 const eNumero = (numero) => /^[0-9]+$/.test(numero);
 // Verifica o tamanho do CEP
 const cepValido = (cep) => cep.length == 8 && eNumero(cep);
+
+// Função para preencher campos relacionados ao CEP
+const preencherFormulario = (endereco) =>{
+    document.getElementById('rua').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('localidade').value = endereco.localidade;
+    document.getElementById('uf').value = endereco.uf;
+}
