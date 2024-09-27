eb1bcb1f97e1cc00d6ae556f1eecaf0776f836f1
nicolas-tonassi <nicolas.tonassi@alunoescolassociais.org.br>
2024-09-27 07:11:13 -0300

    correção de códigos no JavaScript

--- endereco.js
+++ endereco.js
@@ -6,10 +6,10 @@
 // Limpar consulta do form já realizada
 const limparFormulario = () =>{
  
-    document.getElementById('Rua').value = '';
-    document.getElementById('Bairro').value = '';
-    document.getElementById('Cidade').value = '';
-    document.getElementById('Estado').value = '';
+    document.getElementById('logradouro').value = '';
+    document.getElementById('bairro').value = '';
+    document.getElementById('localidade').value = '';
+    document.getElementById('uf').value = '';
  
  
 }
@@ -21,8 +21,30 @@ const cepValido = (cep) => cep.length == 8 && eNumero(cep);
 
 // Função para preencher campos relacionados ao CEP
 const preencherFormulario = (endereco) =>{
-    document.getElementById('rua').value = endereco.logradouro;
+    document.getElementById('logradouro').value = endereco.logradouro;
     document.getElementById('bairro').value = endereco.bairro;
     document.getElementById('localidade').value = endereco.localidade;
     document.getElementById('uf').value = endereco.uf;
 }
+
+// Função para consumo de API ViaCEP
+const pesquisarCep = async() => {
+    limparFormulario();
+    const url = `http://viacep.com.br/ws/${cep.value}/json/`;
+    if(cepValido(cep.value)){
+        const dados = await fetch(url);
+        const addres = await dados.json();
+
+        if(addres.hasOwnProperty()){
+            alert('CEP não encontrado');
+        
+        }else{
+            preencherFormulario(addres);
+        }
+    }else{
+        alert('CEP Incorreto');
+    }
+}
+
+// Executa a ação de preenchimento do formulário ao deixar o campo do CEP
+document.getElementById('cep').addEventListener('focusout' , pesquisarCep);
